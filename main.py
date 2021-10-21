2ee97175d9f631ce661715db3caec5fe27e59b0b
shailavij <shailunatarajan@gmail.com>
2021-10-21 15:46:18 +0100

    First Commit

--- main.py
+++ main.py
@@ -6,12 +6,13 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('shailaja')
-    print("Welcome to new IDE")
+    print("Welcome to new IDE pycharm")
+    print("Welcome to spark")
 
 
